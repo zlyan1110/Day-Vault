8d19df97c2456e1d3b964547c6902d01994e9b96
zlyan1110 <yanzhaolong2000@gmail.com>
2026-03-13 02:32:53 -0700

    supabase client

--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -3,4 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    detectSessionInUrl: true,
+    flowType: "implicit",
+  },
+});
